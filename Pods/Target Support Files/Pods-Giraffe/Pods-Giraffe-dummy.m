6e49cafbb2359130039b08c5abd7ca3ea6665f97
Sergey Maslov <sergey.maslov@rokolabs.com>
2016-10-15 16:27:29 +0300

    cosmetic

---
+++ Pods/Target Support Files/Pods-Giraffe/Pods-Giraffe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Giraffe : NSObject
+@end
+@implementation PodsDummy_Pods_Giraffe
+@end
---
+++ Pods/Target Support Files/Pods-Giraffe/Pods-Giraffe-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GiraffeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GiraffeVersionString[];
+
